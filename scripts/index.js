85ab6461716fe6c6644a82d067cdcd6e6f924aee
coderkearns <coder.kearns@gmail.com>
2022-09-12 09:47:15 -0500

    lesson 01: homepage

---
+++ scripts/index.js
@@ -0,0 +1,1 @@
+document.querySelector("#footercopyright").innerText = new Date().getFullYear()
---
+++ scripts/lastupdated.js
@@ -0,0 +1,3 @@
+// GENERATED DYNAMICALLY - DO NOT EDIT
+const lastUpdated = "09/12/2022"
+document.querySelector("#lastupdated").innerText = lastUpdated
