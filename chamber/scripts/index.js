9de6f4639cf82ef01bdb0519a3f869f5880b5854
coderkearns <coder.kearns@gmail.com>
2022-10-11 13:58:25 -0500

    modifications(week05): Add chamber site content

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -1,4 +1,6 @@
-document.getElementById('date').innerText = (new Date()).toLocaleDateString("en-us", {
+const today = new Date()
+
+document.getElementById('date').innerText = "Today is " + today.toLocaleDateString("en-us", {
     weekday: "long",
     year: "numeric",
     month: "long",
@@ -7,3 +9,7 @@ document.getElementById('date').innerText = (new Date()).toLocaleDateString("en-
 
 document.getElementById("footercopyright").innerText = (new Date()).getFullYear()
 document.getElementById("lastupdated").innerText = document.lastModified
+
+if ([1, 2].includes(today.getDay())) {
+    document.getElementById('date').innerHTML = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m."
+}
